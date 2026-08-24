d287d4d602f248b41a75c546b80f81ea26da95b3
shaanrxx <shaanr@hotmail.co.uk>
2026-08-24 16:07:54 +0100

    Configure GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import faviconUrl from './assets/favicon-32x32.png'
+
+const favicon = document.createElement('link')
+favicon.rel = 'icon'
+favicon.type = 'image/png'
+favicon.sizes = '32x32'
+favicon.href = faviconUrl
+document.head.appendChild(favicon)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
